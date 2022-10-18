a8918fb41f944c97c591360c7af001289d436a31
Jorman <jorman.iisrael@me.com>
2022-10-18 10:11:21 -0400

    added new func to contact

---
+++ src/components/modal.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function Sent() {
+  const values = ['sm-down',];
+  const [fullscreen, setFullscreen] = useState(true);
+  const [show, setShow] = useState(false);
+
+  function handleShow(breakpoint) {
+    setFullscreen(breakpoint);
+    setShow(true);
+  }
+
+  return (
+    <>
+      {values.map((v, idx) => (
+        <Button key={idx} className="me-2 mb-2" onClick={() => handleShow(v)}>
+          Full screen
+          {typeof v === 'string' && `below ${v.split('-')[0]}`}
+        </Button>
+      ))}
+      <Modal show={show} fullscreen={fullscreen} onHide={() => setShow(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Modal body content</Modal.Body>
+      </Modal>
+    </>
+  );
+}
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,30 +1,114 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Row from 'react-bootstrap/Row';
 import ServiceList from '../data';
+import Sent from '../components/modal';
 import '../index.css'
+import axios from 'axios';
 import {HiOutlineMail} from 'react-icons/hi'
 
-const  Contact = ()=> {
-  const [validated, setValidated] = useState(false);
-  const [services]=useState(ServiceList)
+export default class  Contact extends Component{
+  
+  
+   
+   
+  state={ name:'',
+    lastname:'',
+    email:'',
+    message:'',
+    phone:'',
+    services:'',
+    sent:false,
+    validated:false
+  }
+   handleName = (e) =>{
+    this.setState({
+      name:e.target.value
+    })
+   }
+   handleLastName = (e) =>{
+    this.setState({
+      lastname:e.target.value
+    })
+   }
+   handleEmail = (e) =>{
+    this.setState({
+      email:e.target.value
+    })
+   }
+   handlePhone = (e) =>{
+    this.setState({
+      phone:e.target.value
+    })
+   }
+   handleServices =(e)=>{
+    this.setState({
+      services:e.target.value
+    })
+   }
+   handleMessage = (e) =>{
+     this.setState({
+      message:e.target.value
+     })
+  }
+   
+  
+    handleSubmit = async(event) => {
+      
+   
 
+     let data ={
+    name:this.state.name,
+    lastname:this.state.lastname,
+    email:this.state.lastname,
+    message:this.state.message,
+    phone:this.state.phone,
+    service:this.state.services
 
-  const handleSubmit = (event) => {
+  }
+   
+  try {
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
       event.stopPropagation();
+      
     }
+     this.setState({
+      validated:true
+     })
+     if(!this.state.validated){
+      console.log('hii');
+     }else{
+     
+    await axios.post('/api/form' ,data,res =>{
+      
+    },
+    
+     this.setState({
+        sent:true,
+    }))
+  } }catch (error) {
+    console.log('message not sent');
+  }
+  
+   
 
-    setValidated(true);
-  };
+  }
 
+
+
+  
+
+
+
+   
+render(){
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit} className='p-3'>
+    <Form noValidate validated={this.state.validated}  onSubmit={this.handleSubmit} className='p-3'>
       <Row className="mb-3">
         <Form.Group as={Col} md="4" controlId="validationCustom01">
           <Form.Label>First name</Form.Label>
@@ -32,6 +116,9 @@ const  Contact = ()=> {
             required
             type="text"
             placeholder="First name"
+            value={this.state.name}
+            onChange={this.handleName}
+         
             
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
@@ -42,6 +129,8 @@ const  Contact = ()=> {
             required
             type="text"
             placeholder="Last name"
+            value={this.state.lastname}
+            onChange={this.handleLastName}
             
           />
           <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
@@ -54,7 +143,11 @@ const  Contact = ()=> {
               type="email"
               placeholder="Email"
               aria-describedby="inputGroupPrepend"
-              required
+              
+              value={this.state.email}
+            onChange={this.handleEmail}
+            required
+              
             />
             <Form.Control.Feedback type="invalid">
               Please provide a valid your email.
@@ -72,10 +165,11 @@ const  Contact = ()=> {
         </Form.Group>
         <Form.Group as={Col} md="3" controlId="validationCustom04">
           <Form.Label>Services</Form.Label>
-            <Form.Select aria-label="Default select example" required>
+            <Form.Select aria-label="Default select example" required value={this.state.services}
+            onChange={this.handleServices}>
       <option value={''}>Select a Service</option>
-      {services.map(({service,id})=>{
-        return<option value={id} key={id} className='capi'>{service}</option>
+      {ServiceList.map(({service,id})=>{
+        return<option value={service} key={id} className='capi'>{service}</option>
       })}
       
     </Form.Select>
@@ -87,15 +181,16 @@ const  Contact = ()=> {
       </Row>
       <Form.Group as={Col} md="4" controlId="validationCustom05" className='mb-2'>
           <Form.Label>Message</Form.Label>
-          <Form.Control as="textarea" rows={3}placeholder="Comment" required />
+          <Form.Control as="textarea" rows={3}placeholder="Comment" required value={this.state.message}
+            onChange={this.handleMessage} />
           <Form.Control.Feedback type="invalid">
             Please provide a Comment.
           </Form.Control.Feedback>
         </Form.Group>
       
-      <Button type="submit" variant='warning'>Submit form</Button>
+      <Button type="submit" variant='warning' >Submit form</Button>
     </Form>
   );
+    }
 }
 
-export default Contact;
